00815a96801bbc84eedfb25db957170ec577666a
Igor Mitrinovic <devfamilynis@gmail.com>
2026-02-25 08:47:17 +0100

    fix:email logo position

--- resources/views/emails/magic-login-link.blade.php
+++ resources/views/emails/magic-login-link.blade.php
@@ -1,9 +1,8 @@
 <x-mail::message>
-<p align="center">
-    <img src="{{ asset('app_logo.png') }}" alt="Limitless logo" width="160">
-</p>
-
-# Prijava na aplikaciju
+<div style="text-align: center; margin-bottom: 20px;">
+    <img src="{{ asset('app_logo.png') }}" alt="Limitless logo" width="160" style="display: inline-block;">
+    <h1 style="margin: 16px 0 0; font-size: 40px; line-height: 1.2; font-weight: 700; color: #111827;">Prijava na aplikaciju</h1>
+</div>
 
 Klikni na dugme ispod da se prijaviš bez lozinke. Link važi 10 minuta i može da se iskoristi samo jednom.
 
